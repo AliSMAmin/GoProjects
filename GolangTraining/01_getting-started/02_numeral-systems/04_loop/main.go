bd5d7c867db970f7731538367b7be74fc2e9254d
Ali Amin <ali@aliamin.info>
2022-08-04 10:55:40 -0400

    Lesson 52

--- GolangTraining/01_getting-started/02_numeral-systems/04_loop/main.go
+++ GolangTraining/01_getting-started/02_numeral-systems/04_loop/main.go
@@ -3,6 +3,13 @@ package main
 import "fmt"
 
 func main() {
+	a := (42 == 43)
+	b := (42 <= 43)
+	c := (42 >= 43)
+	d := (42 != 43)
+	e := (42 < 43)
+	f := (42 > 43)
+	fmt.Println(a, b, c, d, e, f)
 	for i := 1000000; i < 1000100; i++ {
 		fmt.Printf("%d \t %b \t %x \n", i, i, i)
 	}
