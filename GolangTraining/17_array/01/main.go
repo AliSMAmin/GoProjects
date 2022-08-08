a694e47451132c3014c9066920addfe56d3fec78
Ali Amin <ali@aliamin.info>
2022-08-08 12:41:40 -0400

    Lesson 80

--- GolangTraining/17_array/01/main.go
+++ GolangTraining/17_array/01/main.go
@@ -1,12 +1,12 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+)
 
-func main() {
-	var x [58]int
+func main(){
+	var x [5]int
 	fmt.Println(x)
-	fmt.Println(len(x))
-	fmt.Println(x[42])
-	x[42] = 777
-	fmt.Println(x[42])
+	x [3] = 42; 	fmt.Println(x)
+
 }
