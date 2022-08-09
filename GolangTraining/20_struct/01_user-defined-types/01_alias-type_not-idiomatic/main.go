20ef1e7dff729ec2ab1938c0e31cf3934444a358
Ali Amin <ali@aliamin.info>
2022-08-09 11:19:58 -0400

    Lesson 101

--- GolangTraining/20_struct/01_user-defined-types/01_alias-type_not-idiomatic/main.go
+++ GolangTraining/20_struct/01_user-defined-types/01_alias-type_not-idiomatic/main.go
@@ -1,11 +1,33 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+)
 
-type foo int
-
+type person struct {
+	first string
+	last  string
+	age   int
+}
+	type secretAgent struct {
+		person
+		ltk bool
+}
 func main() {
-	var myAge foo
-	myAge = 44
-	fmt.Printf("%T %v \n", myAge, myAge)
+	sa1 := secretAgent{
+		person:	person{
+		first: "James",
+		last: "Bond",
+		age: 32,
+	},
+		ltk: true,
+	}
+	p2 := person{
+		first: "Miss",
+		last: "Money",
+		age: 18,
+	}
+	fmt.Println(sa1.first, sa1.last, sa1.age, sa1.ltk)
+	fmt.Println(p2)
+
 }
