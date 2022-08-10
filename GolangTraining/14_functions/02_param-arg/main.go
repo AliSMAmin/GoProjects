3e530c01afe1880c0a021fdc9e639d65f443aa3a
Ali Amin <ali@aliamin.info>
2022-08-10 13:07:17 -0400

    Lesson 109

--- GolangTraining/14_functions/02_param-arg/main.go
+++ GolangTraining/14_functions/02_param-arg/main.go
@@ -1,15 +1,37 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+)
 
 func main() {
-	greet("Jane")
-	greet("John")
+	foo()
+	bar("James")
+	st := woo("Moneypenny")
+	fmt.Println(st)
+	x, y := mouse("Ian", " Flemming")
+	fmt.Println(x)
+	fmt.Println(y)
+
 }
 
-func greet(name string) {
-	fmt.Println(name)
+// func (r receiver) identifier(parameters) (return(s)) {...}
+
+func foo() {
+	fmt.Println("hello from foo")
 }
 
-// greet is declared with a parameter
-// when calling greet, pass in an argument
+func bar(s string) {
+	fmt.Println("Hello,", s)
+}
+
+func woo(st string) string {
+	return fmt.Sprint("Hello from woo", st)
+}
+
+func mouse(fn string, ln string) (string, bool) {
+	a := fmt.Sprint(fn, ln, ", says 'Hello'")
+	b := false
+	return a, b
+
+}
