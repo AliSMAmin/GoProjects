d8e933a3804a2bb9fbbe7bef26c665adff80d271
Ali Amin <ali@aliamin.info>
2022-08-10 13:25:04 -0400

    Lesson 111 Unfurling

--- GolangTraining/14_functions/03_two-params/02/main.go
+++ GolangTraining/14_functions/03_two-params/02/main.go
@@ -3,9 +3,17 @@ package main
 import "fmt"
 
 func main() {
-	greet("Jane", "Doe")
+	xi := []int{2, 3, 4, 5, 6, 7}
+	x := sum(xi...)
+	fmt.Println("The total is", x)
 }
-
-func greet(fname, lname string) {
-	fmt.Println(fname, lname)
+func sum(x ...int) int {
+	fmt.Println(x)
+	fmt.Printf("%T\n", x)
+	sum := 0
+	for i, v := range x {
+		sum += v
+		fmt.Println("for item in index position", i, "we are now adding", v, "to the total.")
+	}
+	return sum
 }
