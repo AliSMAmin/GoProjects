0ff4ac9b918232c9c2b81e08b589d90657d1b21d
Ali Amin <ali@aliamin.info>
2022-08-30 12:12:04 -0400

    Hello world tests

--- Hello YOU/hello.go
+++ Hello YOU/hello.go
@@ -3,10 +3,18 @@ package main
 import "fmt"
 
 const englishHelloPrefix = "Hello, "
+const spanishHelloPrefix = "Hola, "
+const spanish = "Spanish"
 
-func Hello(name string) string {
+func Hello(name string, language string) string {
+	if name == "" {
+		name = "World"
+	}
+	if language == spanish {
+		return spanishHelloPrefix
+	}
 	return englishHelloPrefix + name + "!"
 }
 func main() {
-	fmt.Println(Hello("world"))
+	fmt.Println(Hello("World"))
 }
